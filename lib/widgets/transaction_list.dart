4f41e03988b68b610e4db91cf93d22c8f74aad48
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-02-25 09:55:31 +0600

    listview builder used instead of only listvew

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -6,50 +6,49 @@ class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
-  // print(transactions);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 350,
-      child: SingleChildScrollView(
-        child: Column(
-            children: transactions.map((tx) {
-          return Card(
-            elevation: 5,
-            child: Row(
-              children: <Widget>[
-                Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 3)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$ ${tx.amount}',
-                      style: TextStyle(
-                          color: Colors.purple,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    )),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tx.title,
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(color: Colors.grey, fontSize: 15),
-                    )
-                  ],
-                )
-              ],
-            ),
-          );
-        }).toList()),
-      ),
-    );
+        height: 350,
+        child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            return Card(
+              elevation: 5,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 3)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$ ${transactions[index].amount}',
+                        style: TextStyle(
+                            color: Colors.purple,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        style: TextStyle(color: Colors.grey, fontSize: 15),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+        ));
   }
 }
